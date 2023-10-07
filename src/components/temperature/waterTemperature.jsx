68555a93f4bfff5ec8fc45cd9e36d47fad28d3be
thao <mrtrados@gmail.com>
2023-10-07 22:37:14 +0900

    CREATE water's teperature component

---
+++ src/components/temperature/waterTemperature.jsx
@@ -0,0 +1,10 @@
+export default function DisplayWaterTemperature() {
+  return (
+    <div className="bg-amber-100 rounded-lg box-content h-56 w-auto ml-10 mr-10 mb-10">
+      <span>WATER TEMPERATURE</span>
+      <div className="w-60 float-right">
+        <img src="/cold-water-sea.svg" alt="watertemperature" />
+      </div>
+    </div>
+  );
+}
