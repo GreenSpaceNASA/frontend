f8fe31d792e5bda22ed6516732064bee7452d695
thao <mrtrados@gmail.com>
2023-10-08 12:09:40 +0900

    UPDATE distance, currentPlace, aquaticArea

--- src/components/distanceCount.jsx
+++ src/components/distanceCount.jsx
@@ -1,12 +1,12 @@
 
 
-const distance = "5";
-export default function CountDistance() {
+export default function CountDistance({aquaticArea, distance, currentLocation}) {
   return (
     <div>
       <div className="bg-sky-200 rounded-lg box-content h-48 w-auto ml-10 mr-10 mb-10">
-        <p className="text-7xl hover:text-8xl text-center">
-          You are {distance} km from the sea!
+        <p className="text-6xl hover:text-7xl text-center">
+          You are at <strong>{currentLocation}</strong> 
+          and <strong>{distance}</strong> km away from <strong>{aquaticArea}</strong>!
         </p>
       </div>
     </div>
