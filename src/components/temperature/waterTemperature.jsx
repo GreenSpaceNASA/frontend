2107c02ae35143957a372cad51437993febad9c9
thao <mrtrados@gmail.com>
2023-10-08 12:10:54 +0900

    UPDATE with real temp

--- src/components/temperature/waterTemperature.jsx
+++ src/components/temperature/waterTemperature.jsx
@@ -1,9 +1,11 @@
-export default function DisplayWaterTemperature() {
+
+
+export default function DisplayWaterTemperature({tempCelcius}) {
   return (
-    <div className="bg-amber-100 rounded-lg box-content h-56 w-auto ml-10 mr-10 mb-10">
-      <span>WATER TEMPERATURE</span>
-      <div className="w-60 float-right">
-        <img src="/cold-water-sea.svg" alt="watertemperature" />
+    <div className="bg-amber-100 rounded-lg box-content h-60 w-[500px] ml-20 mr-20 mb-10">
+      <span>Current temperature of water <strong>{tempCelcius}</strong></span>.
+      <div className="w-60 float-right hover:rounded-2xl">
+        <img src="/cold-water.svg" alt="watertemperature" />
       </div>
     </div>
   );
