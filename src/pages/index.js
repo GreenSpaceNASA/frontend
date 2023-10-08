2470252984f6391ddda53ae400b8eb4a070335b5
thao <mrtrados@gmail.com>
2023-10-08 12:08:44 +0900

    UPDATE index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,27 @@ import CountDistance from '@/components/distanceCount';
 import ContaminateLevel from '@/components/chart/contaminateChart';
 import DisplayWaterTemperature
  from '@/components/temperature/waterTemperature';
+import getArea from '@/lib/getArea';
+import getAnimal from '@/lib/getAnimal';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export async function getServerSideProps() {
+  const area = await getArea();
+  const animals = await getAnimal();
+  return {props: {area, animals}}
+}
+
+export default function Home({area, animals}) {
   return (
     <>
-    <DisplayIconStatus/>
-    <CountDistance/>
+    <DisplayIconStatus waterStt={area.nearest_aquatic_location.condition}/>
+    <CountDistance 
+    aquaticArea = {area.nearest_aquatic_location.name}
+    distance={area.nearest_aquatic_location.distance} 
+    currentLocation={area.location.name}/>
     <ContaminateLevel/>
-    <DisplayWaterTemperature/>
+    <DisplayWaterTemperature tempCelcius={area.nearest_aquatic_location.temperature}/>
     </>
   )
 }
