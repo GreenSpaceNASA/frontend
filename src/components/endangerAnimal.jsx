709ec76c1bff8829d89394be5e5c60542a3b277e
thao <mrtrados@gmail.com>
2023-10-29 23:53:04 +0900

    UPDATE useState(put outside of callback hook)

--- src/components/endangerAnimal.jsx
+++ src/components/endangerAnimal.jsx
@@ -1,45 +1,50 @@
-import { faBiohazard } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShield } from "@fortawesome/free-solid-svg-icons";
 import { faShrimp } from "@fortawesome/free-solid-svg-icons";
 import { faFish } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
-export default function DisplayAnimals({ animalsList}) {
-
-  const animalListJSX = animalsList.map((item) => {
+export default function DisplayAnimals({ animalsList }) {
   const [showMe, setShowMe] = useState(false);
-  function toggle(){
+  function toggle() {
     setShowMe(!showMe);
   }
+  const animalListJSX = animalsList.map((item) => {
     return (
-        <li key={item.name} className="font-bold py-4">
-          <button onClick={toggle}>
-          { item.in_danger &&(<span>
-          <FontAwesomeIcon icon={faShield} className="text-2xl text-sky-300"/>
-          </span>)}{item.name} <br />
-          </button>
-          <div className="font-normal text-xl py-4 px-6" style={{
-        display: showMe?"block":"none"
-      }}>
-        {item.description}
-      </div>
-        </li>
+      <li key={item.name} className="font-bold py-4">
+        <button onClick={toggle}>
+          {item.in_danger && (
+            <span>
+              <FontAwesomeIcon
+                icon={faShield}
+                className="text-2xl text-sky-300"
+              />
+            </span>
+          )}
+          {item.name} <br />
+        </button>
+        <div
+          className="font-normal text-xl py-4 px-6"
+          style={{
+            display: showMe ? "block" : "none",
+          }}
+        >
+          {item.description}
+        </div>
+      </li>
     );
   });
 
   return (
     <div className="text-xl text-sky-200 w-auto m-auto mt-10 mb-20 text-center">
       <div className="inline-flex items-center justify-center w-full opacity-20">
-    <hr className="w-64 h-1 my-8 bg-blue-900 border-0 rounded"/>
-    <div className="absolute px-4 -translate-x-1/2 left-1/2">
+        <hr className="w-64 h-1 my-8 bg-blue-900 border-0 rounded" />
+        <div className="absolute px-4 -translate-x-1/2 left-1/2">
           {/* <FontAwesomeIcon icon={faShrimp} className="text-2xl text-blue-900"/> */}
-          <FontAwesomeIcon icon={faFish} className="text-2xl text-blue-900"/>
-    </div>
-    </div>
-      <ul className="">
-        {animalListJSX}
-      </ul>
+          <FontAwesomeIcon icon={faFish} className="text-2xl text-blue-900" />
+        </div>
+      </div>
+      <ul className="">{animalListJSX}</ul>
     </div>
   );
 }
